50816b168006354e487fa837cba178bee6f172b7
Wilson <wilsoncoutinh@gmail.com>
2024-05-31 20:11:57 -0300

    Sign

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import SignIn from "./pages/Login/SignIn";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SignIn />
+    </div>
+
+  );
+}
+
+export default App;
